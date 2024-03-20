466a4924c6c6db723ba06f42bc8c5cc52cf2f4c3
TasinFarhanMC <tasinfarhan1016@gmail.com>
2024-03-20 15:10:13 +0600

    Finished Input

--- sft_input.h
+++ sft_input.h
@@ -7,10 +7,10 @@
 bool sft_input_scans(char* buffer, size_t size);
 bool sft_input_fscans(FILE* file, char* buffer, size_t size);
 
-int sft_input_gets(char* buffer, size_t size, bool inc_newline);
-int sft_input_fgets(FILE* file, char* buffer, size_t size, bool inc_newline);
+bool sft_input_gets(char* buffer, size_t size, bool inc_newline);
+bool sft_input_fgets(char* buffer, size_t size, bool inc_newline, FILE* file);
 
-#ifdef SFT_INPUT_IMP  
+#ifdef SFT_INPUT_IMP
 #include <string.h>
 
 bool sft_input_scans(char* buffer, const size_t size) {
@@ -36,5 +36,60 @@ bool sft_input_fscans(FILE* file, char* buffer, const size_t size) {
 
   return true;
 }
+
+bool sft_input_gets(char* buffer, size_t size, bool inc_newline) {
+  buffer[size - 2] = 0; 
+  fgets(buffer, size, stdin);
+
+  if (inc_newline) {
+    if (buffer[size - 2] == 0 || buffer[size - 2] == '\n') {
+      return true;
+    } else {
+      scanf("%*s");
+      return false;
+    }
+  } else {
+    if (buffer[size - 2] == '\n') {
+      buffer[size - 2] = 0;
+      return true;
+    }
+
+    if (buffer[size - 2] == 0) {
+      buffer[strlen(buffer) - 2] = 0;
+      return true;
+    } else {
+      scanf("%*s");
+      return false;
+    }
+  }
+}
+
+bool sft_input_fgets(char* buffer, size_t size, bool inc_newline, FILE* file) {
+  buffer[size - 2] = 0; 
+  fgets(buffer, size, file);
+
+  if (inc_newline) {
+    if (buffer[size - 2] == 0 || buffer[size - 2] == '\n') {
+      return true;
+    } else {
+      scanf("%*s");
+      return false;
+    }
+  } else {
+    if (buffer[size - 2] == '\n') {
+      buffer[size - 2] = 0;
+      return true;
+    }
+
+    if (buffer[size - 2] == 0) {
+      buffer[strlen(buffer) - 2] = 0;
+      return true;
+    } else {
+      scanf("%*s");
+      return false;
+    }
+  }
+}
+
 #endif
 #endif
